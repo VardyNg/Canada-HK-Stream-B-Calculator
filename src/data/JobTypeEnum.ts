c7b264f3477ab7a670ff5199dfeb3f3864d0e1e9
vardy.ng <wadee@wadeenghoiwa.com>
2024-07-19 18:40:50 -0400

    add JobType enum:

---
+++ src/data/JobTypeEnum.ts
@@ -0,0 +1,6 @@
+enum JobTypeEnum {
+  fulltime,
+  parttime
+}
+
+export default JobTypeEnum;
--- src/data/index.ts
+++ src/data/index.ts
@@ -3,10 +3,13 @@ import * as Constants from './Constants'
 import PassportTypeEnum from './PassportTypeEnum';
 import LanguageTestTypeEnum from './LanguageTestTypeEnum';
 import LanguageTestSpec from './LanguageTestSpec';
+import JobTypeEnum from './JobTypeEnum';
+
 export {
   LocalStorageEnum,
   Constants,
   PassportTypeEnum,
   LanguageTestTypeEnum,
   LanguageTestSpec,
+  JobTypeEnum,
 }
