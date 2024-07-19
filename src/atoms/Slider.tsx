a733abc6dfb0941fb4bfaf02b44fe75fd6d2e276
vardy.ng <wadee@wadeenghoiwa.com>
2024-07-19 18:09:20 -0400

    add value, error, and onChange props, change slider color and use onChange

--- src/atoms/Slider.tsx
+++ src/atoms/Slider.tsx
@@ -3,7 +3,10 @@ import { Slider as SliderMui } from '@mui/material';
 type SliderProp = {
   min: number,
   max: number,
-  step: number
+  step: number,
+  value: number,
+  error: boolean,
+  onChange: Function,
 }
 function Slider(props: SliderProp) {
   console.log(props);
@@ -25,6 +28,8 @@ function Slider(props: SliderProp) {
       step={props.step}
       marks={marks}
       valueLabelDisplay="auto"
+      color={props.error ? "error" : "primary"}
+      onChange={(event, newValue) => {props.onChange(newValue)}}
     />
   )
 }
