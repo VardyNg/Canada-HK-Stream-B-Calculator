4878834d00bff94c0020c1b2745de55336f00430
vardy.ng <wadee@wadeenghoiwa.com>
2024-07-19 08:58:39 -0400

    add max and min value display

--- src/atoms/Slider.tsx
+++ src/atoms/Slider.tsx
@@ -6,12 +6,25 @@ type SliderProp = {
   step: number
 }
 function Slider(props: SliderProp) {
+  console.log(props);
+  const marks = [
+    {
+      value: props.min,
+      label: props.min
+    },
+    {
+      value: props.max,
+      label: props.max
+    },
+  ]
   return (
     <SliderMui 
       orientation="vertical"
       min={props.min}
       max={props.max}
       step={props.step}
+      marks={marks}
+      valueLabelDisplay="auto"
     />
   )
 }
