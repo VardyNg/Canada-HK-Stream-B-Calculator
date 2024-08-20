ca8b852910583dcd0b7eee6d92e8f17033138239
vardy.ng <wadee@wadeenghoiwa.com>
2024-08-19 23:13:11 -0400

    Add owp validity max and min date

--- src/molecules/stream-b-calculator/wp-expiry-date.tsx
+++ src/molecules/stream-b-calculator/wp-expiry-date.tsx
@@ -4,18 +4,27 @@ import {
 import {
   LocalStorageEnum,
 } from '../../data';
-
+import moment from 'moment';
 type WPExpriyDateDate = {
   onChange: Function
 }
 
 function WPExpriyDate(props: WPExpriyDateDate) {
+  const minDate = 
+    moment('2021-06-01'); // open work permit starts on this date
+
+  const maxDate = 
+    moment('2025-02-07') // open work permit application ends on this date
+      .add(3, 'years'); // the maximum permit validity is 3 years 
+  
   
   return (
     <DatePicker
       label="工作簽證有效日期 Work permit expiry date"
       localStorageKey={LocalStorageEnum.WorkPermitExpiryDate}
       onChange={props.onChange}
+      minDate={minDate.toDate()}
+      maxDate={maxDate.toDate()}
     />
   )
 }
