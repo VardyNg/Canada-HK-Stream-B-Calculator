519aeb0276146dc52bfa72dd8beb2396bf4f93fd
vardy.ng <wadee@wadeenghoiwa.com>
2024-08-21 21:44:41 -0400

    set job type min width

--- src/molecules/stream-b-calculator/job-type-picker.tsx
+++ src/molecules/stream-b-calculator/job-type-picker.tsx
@@ -3,15 +3,26 @@ import MenuItem from '@mui/material/MenuItem';
 import {
   JobTypeEnum
 } from '../../data';
+import InputLabel from '@mui/material/InputLabel';
+import FormControl from '@mui/material/FormControl';
+import Box from '@mui/material/Box';
 
 function JobTypePicker() {
   return (
-    <Select
-      label="工作類型 Job Category"
+    <FormControl
+      style={{
+        minWidth: 230
+      }}
     >
-      <MenuItem value={JobTypeEnum.fulltime}>全職 Fulltime</MenuItem>
-      <MenuItem value={JobTypeEnum.parttime}>兼職 Parttime</MenuItem>
-    </Select>
+      <InputLabel id="demo-simple-select-label">工作類型 Job Category</InputLabel>
+      <Select
+        label="工作類型 Job Category"
+        autoWidth
+      >
+        <MenuItem value={JobTypeEnum.fulltime}>全職 Fulltime</MenuItem>
+        <MenuItem value={JobTypeEnum.parttime}>兼職 Parttime</MenuItem>
+      </Select>
+    </FormControl>
   )
 }
 
