9a48db7b1f999a2fd04f8757de25bf42e10d3dab
vardy.ng <wadee@wadeenghoiwa.com>
2024-08-21 22:00:22 -0400

    update working hours summary fields

--- src/organisms/stream-b-calculator/working-hours-summary.tsx
+++ src/organisms/stream-b-calculator/working-hours-summary.tsx
@@ -7,21 +7,19 @@ import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 
 function createData(
-  name: string,
-  calories: number,
-  fat: number,
-  carbs: number,
-  protein: number,
+  year: number,
+  week_no: number,
+  working_hours: number,
+  effective_working_hours: number,
+  equivalent_week_no: number,
 ) {
-  return { name, calories, fat, carbs, protein };
+  return { year, week_no, working_hours, effective_working_hours, equivalent_week_no };
 }
 
 const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
+  createData(2022, 5, 45, 30, 1),
+  createData(2022, 4, 28, 28, 0.93333),
+  createData(2022, 4, 14, 14, 0.46666),
 ];
 
 function WorkingHoursSummary() {
@@ -31,26 +29,26 @@ function WorkingHoursSummary() {
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
           <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+            <TableCell>年 Year</TableCell>
+            <TableCell align="right">周 week</TableCell>
+            <TableCell align="right">工作時數 Working Hours</TableCell>
+            <TableCell align="right">有效工作時數 Effective Working Hours</TableCell>
+            <TableCell align="right">對應週數 Equivalent No. of Week</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
           {rows.map((row) => (
             <TableRow
-              key={row.name}
+              key={row.year}
               sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
             >
               <TableCell component="th" scope="row">
-                {row.name}
+                {row.year}
               </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
+              <TableCell align="right">{row.week_no}</TableCell>
+              <TableCell align="right">{row.working_hours}</TableCell>
+              <TableCell align="right">{row.effective_working_hours}</TableCell>
+              <TableCell align="right">{row.equivalent_week_no}</TableCell>
             </TableRow>
           ))}
         </TableBody>
